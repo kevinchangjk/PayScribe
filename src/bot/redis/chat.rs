ba8413be81e132efc7ddf11d6dd67b7cc3f8bd25
kevinchangjk <kevinchangjk@gmail.com>
2024-03-11 16:21:14 +0800

    redis: add chat crud operations

--- src/bot/redis/chat.rs
+++ src/bot/redis/chat.rs
@@ -8,14 +8,14 @@ const CHAT_KEY: &str = "chat";
 pub fn add_chat(chat_id: &str, user_id: &str) -> redis::RedisResult<()> {
     let mut con = connect();
     let chat: &[String; 1] = &[user_id.to_string()];
-    con.hset(format!("{CHAT_KEY}:{chat_id}"), "users", chat)
+    con.set(format!("{CHAT_KEY}:{chat_id}"), chat)
 }
 
 // Gets all users from a chat
 // Returns a vector of user_id or username
 pub fn get_chat_users(chat_id: &str) -> redis::RedisResult<Vec<String>> {
     let mut con = connect();
-    con.hget(format!("{CHAT_KEY}:{chat_id}"), "users")
+    con.get(format!("{CHAT_KEY}:{chat_id}"))
 }
 
 // Adds a single new user to the chat
@@ -27,7 +27,7 @@ pub fn add_chat_user(chat_id: &str, user: String) -> redis::RedisResult<()> {
     if !current_users.contains(&user) {
         new_users.push(user);
     }
-    con.hset(format!("{CHAT_KEY}:{chat_id}"), "users", current_users)
+    con.set(format!("{CHAT_KEY}:{chat_id}"), current_users)
 }
 
 // Adds more users to the chat
@@ -41,7 +41,24 @@ pub fn add_chat_user_multiple(chat_id: &str, users: Vec<String>) -> redis::Redis
             new_users.push(user);
         }
     }
-    con.hset(format!("{CHAT_KEY}:{chat_id}"), "users", current_users)
+    con.set(format!("{CHAT_KEY}:{chat_id}"), current_users)
+}
+
+// Updates a chat for the initialization of a user
+// Used only in conjunction with user::initialize_user
+pub fn update_chat_user_init(
+    chat_id: &str,
+    username: &str,
+    user_id: &str,
+) -> redis::RedisResult<()> {
+    let mut con = connect();
+    let current_users: Vec<String> = get_chat_users(chat_id)?;
+    let mut new_users = current_users.clone();
+    if current_users.contains(&username.to_string()) {
+        new_users.retain(|x| x != username);
+        new_users.push(user_id.to_string());
+    }
+    con.set(format!("{CHAT_KEY}:{chat_id}"), new_users)
 }
 
 #[cfg(test)]
@@ -50,6 +67,7 @@ mod tests {
     use super::add_chat_user;
     use super::add_chat_user_multiple;
     use super::get_chat_users;
+    use super::update_chat_user_init;
 
     #[test]
     fn test_add_chat() {
@@ -82,5 +100,16 @@ mod tests {
             "987654324".to_string(),
         ];
         assert!(add_chat_user_multiple(chat_id, users).is_ok());
+    }
+
+    #[test]
+    fn test_update_chat_user_init() {
+        let chat_id = "1234567890";
+        let username = "test_user";
+        let user_id = "9876543210";
+        add_chat(chat_id, username).unwrap();
+
+        update_chat_user_init(chat_id, username, user_id).unwrap();
+        assert_eq!(get_chat_users(chat_id).unwrap(), vec![user_id.to_string()]);
     }
 }
