03be4b4d1fc3300f92a1b6cb51db73d0ebdafe2f
kevinchangjk <kevinchangjk@gmail.com>
2024-03-11 17:30:43 +0800

    redis: add balance crud operations

--- src/bot/redis/balance.rs
+++ src/bot/redis/balance.rs
@@ -10,6 +10,14 @@ pub fn add_balance(chat_id: &str, user_id: &str) -> redis::RedisResult<()> {
     con.hset_multiple(format!("{BALANCE_KEY}:{chat_id}:{user_id}"), balance)
 }
 
+// Gets a balance
+pub fn get_balance(chat_id: &str, user_id: &str) -> redis::RedisResult<(i32, i32)> {
+    let mut con = connect();
+    let amount_into: i32 = con.hget(format!("{BALANCE_KEY}:{chat_id}:{user_id}"), "amount_into")?;
+    let amount_from: i32 = con.hget(format!("{BALANCE_KEY}:{chat_id}:{user_id}"), "amount_from")?;
+    Ok((amount_into, amount_from))
+}
+
 // Updates a balance in Redis
 pub fn update_balance(
     chat_id: &str,
@@ -30,6 +38,7 @@ pub fn update_balance(
 #[cfg(test)]
 mod tests {
     use super::add_balance;
+    use super::get_balance;
     use super::update_balance;
 
     #[test]
@@ -40,9 +49,18 @@ mod tests {
     }
 
     #[test]
+    fn test_get_balance() {
+        let chat_id = "1234567890";
+        let user_id = "9876543210";
+        add_balance(chat_id, user_id).unwrap();
+        assert_eq!(get_balance(chat_id, user_id).unwrap(), (0, 0));
+    }
+
+    #[test]
     fn test_update_balance() {
         let chat_id = "123456789";
         let user_id = "987654321";
         assert!(update_balance(chat_id, user_id, 13, 42).is_ok());
+        assert_eq!(get_balance(chat_id, user_id).unwrap(), (13, 42));
     }
 }
